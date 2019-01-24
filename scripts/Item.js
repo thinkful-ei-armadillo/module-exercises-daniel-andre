cb8e616aa1148c0fe2102172a457e229c7badc67
Daniel Ors <do7811@gmail.com>
2019-01-24 11:28:44 -0500

    finished basic module exercises

---
+++ scripts/Item.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-console */
+/* global  */
+'use strict';
+
+const Item = (function () {
+  
+  const workingItem = {};
+  return workingItem;
+} () );
--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,8 @@
-/* global shoppingList, cuid */
-
+/* global shoppingList, $ */
 // eslint-disable-next-line no-unused-vars
-const store = {
-  items: [
-    { id: cuid(), name: 'apples', checked: false },
-    { id: cuid(), name: 'oranges', checked: false },
-    { id: cuid(), name: 'milk', checked: true },
-    { id: cuid(), name: 'bread', checked: false }
-  ],
-  hideCheckedItems: false,
-  searchTerm: ''
-};
+'use strict';
 
-$(document).ready(function() {
-  shoppingList.bindEventListeners();
-  shoppingList.render();
-});
+// $(document).ready(function() {
+//   shoppingList.bindEventListeners();
+//   shoppingList.render();
+// });
