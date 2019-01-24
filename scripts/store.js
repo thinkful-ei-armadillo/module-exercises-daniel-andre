fb563b2f593000ce30b987a32f87473787d51939
Daniel Ors <do7811@gmail.com>
2019-01-24 13:35:05 -0500

    finished first three namespacing drills

--- scripts/store.js
+++ scripts/store.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-empty */
 /* eslint-disable no-console */
 /* global cuid */
 'use strict';
@@ -12,10 +13,56 @@ const store = (function () {
   let hideCheckedItems = false;
   let searchTerm = '';
 
+  let findById = (function (searchId) {
+    items.find( id => { if (searchId === id) {  } });
+  });
+
+  let addItem = (function (name) {
+    try {
+      Item.validateName(name);
+      items.push(Item.create(name));
+    }
+    catch (error) {
+      return error.message;
+    }
+  });
+
+  let findAndToggleChecked = (function (id) {
+    let objectFound = this.findById(id);
+    if ( this.objectFound.checked === true ) {
+      objectFound.checked = false;
+    }
+    else {
+      objectFound.checked = true;
+    }
+  });
+
+  let findAndUpdateName = (function (id, newName) {
+    let objectFound = this.findById(id);
+    try {
+      Item.validateName(newName);
+      objectFound.name = newName;
+    }
+    catch ( error ) {
+      return 'Cannot update name: {error.message}';
+    }
+  });
+
+  let findAndDelete = (function (deleteId) {
+    let objectFound = this.findById(deleteId);
+    let indexOfObjectFound = this.items.findIndex( id => deleteId === id );
+    items.splice(indexOfObjectFound);
+  });
+
   return {       
     items,
     hideCheckedItems,
-    searchTerm
+    searchTerm,
+    findById,
+    addItem,
+    findAndToggleChecked,
+    findAndUpdateName,
+    findAndDelete
   };
 
 } () );
